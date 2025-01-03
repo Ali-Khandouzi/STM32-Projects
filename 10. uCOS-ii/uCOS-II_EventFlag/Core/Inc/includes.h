22415edfe038102ef71cdc293c13addf9c0d8492
Ali-Khandouzi <70533333+Ali-Khandouzi@users.noreply.github.com>
2025-01-03 21:41:40 +0330

    Update

---
+++ 10. uCOS-ii/uCOS-II_EventFlag/Core/Inc/includes.h
@@ -0,0 +1,21 @@
+/*
+ * includes.h
+ *
+ *  Created on: Dec 29, 2024
+ *      Author: Jaam-eJam
+ */
+
+#ifndef INC_INCLUDES_H_
+#define INC_INCLUDES_H_
+
+#include "stm32l4xx_hal.h"
+#include "app_cfg.h"
+#include "os_cfg.h"
+#include "os_cpu.h"
+#include "os.h"
+#include "ucos_ii.h"
+#include "os_trace.h"
+
+
+
+#endif /* INC_INCLUDES_H_ */
